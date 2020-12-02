23e1bc7688e965580cab1eeb2d113743f2695236
Yehia Qtaish -X (yqtaish - HCL AMERICA INC at Cisco) <yqtaish@cisco.com>
2020-12-02 15:53:19 -0800

    Application can send and receive message, now need to add a database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-
+import React, {useState, useEffect} from 'react';
+import {FormControl, InputLabel, Input, Button} from '@material-ui/core';
+import Message from './Components/Message';
 
 function App() {
   const [input, setInput] = useState('');
+  const [messages, setMessage] = useState([
+    {username: "John", text: "test1"},
+    {username: "qazi", text: "test2"}
+  ]);
+  const [username, setUsername] = useState('');
+
+  //Runs the code on a condition
+  // if the input array is empty that means the code will only run once
+  useEffect(() => {
+    setUsername(prompt('Please enter your name'));
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessage([...messages, {username: username, text: input}]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-     <input value ={input} onChange={event => setInput(event.target.value)}/>
-     <button>Send message</button>
+      <h1>Welcome {username}</h1>
+    <form>
+      <FormControl>
+        <InputLabel>Enter Message </InputLabel>
+        <Input value ={input} onChange={event => setInput(event.target.value)}/>
+        <Button disabled={!input} variant="contained" color='primary' type='submit' onClick={sendMessage} >Send message</Button>
+      </FormControl>
+    </form>
+      {
+        messages.map(message => (
+          <Message username={username} message={message} />
+        ))
+      }
+
     </div>
   );
 }
